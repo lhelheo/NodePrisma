0151a63f0b6ab22d7d2a7e24cd048dd567d1f564
Leonardo Oliveira Villalva <leonardo.oliveira.villalva@hotmail.com>
2024-09-18 17:28:31 -0300

    feat: upsert

--- src/services/user.ts
+++ src/services/user.ts
@@ -2,11 +2,14 @@ import { Prisma } from "@prisma/client";
 import { prisma } from "../libs/prisma";
 
 export const createUser = async (data: Prisma.UserCreateInput) => {
-    try {
-        return await prisma.user.create({ data });
-    } catch (error) {
-        return false;
-    }
+    const result = await prisma.user.upsert({
+        where: {
+            email: data.email
+        },
+        update: {},
+        create: data
+    })
+    return result;
 }
 
 export const createUsers = async (users: Prisma.UserCreateInput[]) => {
